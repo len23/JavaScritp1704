f6f4612b299c81a0501b43369844c427e232ec02
len23 <lenin.montalvo.23@hotmail.com>
2018-04-17 15:16:37 -0500

    PrimerosPasos

--- 01-tipos-variables/tipos.js
+++ 01-tipos-variables/tipos.js
@@ -1,10 +1,38 @@
 var nombre = "25";
 var edad = 25;
 var fecha = new Date();
+var casado = false;
+var casa = undefined;
+var carro = null;
+var arreglo = [];
+console.log(casa?"verdadero":"falso");
+console.log(arreglo?"verdadero":"falso");
+console.log(carro?"verdadero":"falso");
+console.log( 0?"verdadero":"falso");
+
+var usuario = {
+    "nombre":"Lenin",
+    "apellido":"Montalvo",
+    cedula:'0401485370',
+    //imprimirEnConsola:
+};
+function sumarDosNumeros(numeroUno,numeroDos){
+    return numeroUno + numeroDos;
+}
+console.log(sumarDosNumeros(1,2));
 
 
-if(nombre===edad){
+console.log("Usuario: ", usuario);
+
+
+
+
+
+console.log(fecha);
+//ctr + alt + l
+
+if (nombre === edad) {
     console.log("Verdadero");
-}else {
+} else {
     console.log("falso");
 }

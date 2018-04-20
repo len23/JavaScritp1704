ba9e42de3d1c6786980c27fc98ef133b1560c2f0
Unknown <lenin.montalvo.23@hotmail.com>
2018-04-20 16:49:31 -0500

    Añadiendo tipos de datos

--- Typescript/01-tipos-variables.js
+++ Typescript/01-tipos-variables.js
@@ -3,3 +3,10 @@ var cedula = '0401485370';
 nombre = 'Santiago';
 //cedula = '0983189385';
 var URL1 = 'http://www.epn.edu.ec';
+nombre = 123; //Duck typing
+//-------------------------------------------
+var nombre1;
+var edad = 28;
+var fecha = new Date();
+var casado = false;
+//let mascotas:object ={} 
--- Typescript/01-tipos-variables.ts
+++ Typescript/01-tipos-variables.ts
@@ -1,8 +1,16 @@
 
-let nombre = 'Lenin';
+let nombre:any= 'Lenin';
 const cedula = '0401485370';
 
 nombre = 'Santiago';
 //cedula = '0983189385';
 
 const URL1 = 'http://www.epn.edu.ec';
+nombre = 123; //Duck typing
+
+//-------------------------------------------
+let nombre1:String;
+let edad: Number = 28;
+let fecha: Date = new Date();
+let casado:boolean = false;
+//let mascotas:object ={}

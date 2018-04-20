c04e4c1eb242cee4c0ca52ffa2aa3c36570d338e
Unknown <lenin.montalvo.23@hotmail.com>
2018-04-20 16:40:42 -0500

    Definiendo variables

---
+++ Typescript/01-tipos-variables.js
@@ -0,0 +1,5 @@
+var nombre = 'Lenin';
+var cedula = '0401485370';
+nombre = 'Santiago';
+//cedula = '0983189385';
+var URL1 = 'http://www.epn.edu.ec';
---
+++ Typescript/01-tipos-variables.ts
@@ -0,0 +1,8 @@
+
+let nombre = 'Lenin';
+const cedula = '0401485370';
+
+nombre = 'Santiago';
+//cedula = '0983189385';
+
+const URL1 = 'http://www.epn.edu.ec';
